ba78cc879236867014e9c9e7a1738cc1834af3c3
igor <igorbelikov2009@yandex.ru>
2022-12-04 15:00:57 +0300

    MyInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
+
 function App() {
-  return <div className="App">A'm working</div>;
+  const [counter] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [posts, setPosts] = useState([
+    { id: 1, title: " Javascript", body: "Javascript - язык программирования" },
+    { id: 2, title: " Javascript 2", body: "Javascript - язык программирования 2" },
+    { id: 3, title: " Javascript 3", body: "Javascript - язык программирования 3" },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+      id: Date.now(),
+      title,
+      body,
+    };
+    // posts.push(newPost); либо, как ниже
+    setPosts([...posts, newPost]);
+
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <div className="App">
+      {title}
+      <form>
+        <MyInput type="text" placeholder="Название поста" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <MyInput type="text" placeholder="Описание поста" value={body} onChange={(e) => setBody(e.target.value)} />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов 1" />
+
+      {counter && <Counter />}
+      {counter && <ClassCounter />}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PostItem = ({ post, number }) => {
+  return (
+    <div className="post">
+      <div className="post__content">
+        <div>
+          <strong>
+            {number}. {post.title}
+          </strong>
+          <div>{post.body} </div>
+        </div>
+      </div>
+
+      <div className="post__btns">
+        <button>Удалить</button>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
